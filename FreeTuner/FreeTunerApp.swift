9625a02af405a8eece9e0e66fb9102ee3c301ba9
Michaellis <michael94ellis@gmail.com>
2025-08-22 16:58:38 -0400

    Initial Commit

---
+++ FreeTuner/FreeTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTunerApp.swift
+//  FreeTuner
+//
+//  Created by Michael Ellis on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
