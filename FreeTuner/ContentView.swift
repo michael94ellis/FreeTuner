37794c21f2df2ae913047ae1710e4a645d67ed7b
Michaellis <michael94ellis@gmail.com>
2025-08-24 19:45:44 -0400

    Fix some view height issues

--- FreeTuner/ContentView.swift
+++ FreeTuner/ContentView.swift
@@ -26,33 +26,9 @@ struct ContentView: View {
     @State private var viewState: ViewState = .tuner
 
     var body: some View {
-        // Tuner Tab
-        VStack {
+        GeometryReader { geo in
             
-            Picker("Mode", selection: $viewState, content: {
-                Label(title: {
-                    Text("Tuner")
-                }, icon: {
-                    Image(systemName: "tuningfork")
-                })
-                .tag(ViewState.tuner)
-                Label(title: {
-                    Text("Metronome")
-                }, icon: {
-                    Image(systemName: "timer")
-                })
-                .tag(ViewState.metronome)
-                Label(title: {
-                    Text("Both")
-                }, icon: {
-                    Image(systemName: "music.note.list")
-                })
-                .tag(ViewState.both)
-            })
-            .pickerStyle(.segmented)
-            
-            switch viewState {
-            case .tuner:
+            let tunerView: some View =
                 TunerView(
                     pitchManager: pitchManager,
                     noteConverter: noteConverter,
@@ -62,42 +38,57 @@ struct ContentView: View {
                     currentSpectrum: $currentSpectrum
                 )
                 .id("tuner")
-            case .metronome:
-                MetronomeView(metronome: metronome)
-                    .id("metronome")
-            case .both:
-                ViewThatFits {
-                    HStack {
-                        TunerView(
-                            pitchManager: pitchManager,
-                            noteConverter: noteConverter,
-                            isListening: $isListening,
-                            errorMessage: $errorMessage,
-                            currentPitch: $currentPitch,
-                            currentSpectrum: $currentSpectrum
-                        )
-                        .id("tuner")
-                        MetronomeView(metronome: metronome)
-                            .id("metronome")
+                .frame(width: geo.size.width, height: geo.size.height)
+            
+            // Tuner Tab
+            VStack {
+                
+                Picker("Mode", selection: $viewState, content: {
+                    Label(title: {
+                        Text("Tuner")
+                    }, icon: {
+                        Image(systemName: "tuningfork")
+                    })
+                    .tag(ViewState.tuner)
+                    Label(title: {
+                        Text("Metronome")
+                    }, icon: {
+                        Image(systemName: "timer")
+                    })
+                    .tag(ViewState.metronome)
+                    Label(title: {
+                        Text("Both")
+                    }, icon: {
+                        Image(systemName: "music.note.list")
+                    })
+                    .tag(ViewState.both)
+                })
+                .pickerStyle(.segmented)
+                
+                switch viewState {
+                case .tuner:
+                    tunerView
+                case .metronome:
+                    MetronomeView(metronome: metronome)
+                        .id("metronome")
+                case .both:
+                    ViewThatFits {
+                        HStack {
+                            tunerView
+                            MetronomeView(metronome: metronome)
+                                .id("metronome")
+                        }
+                        ScrollView {
+                            VStack {
+                                tunerView
+                                MetronomeView(metronome: metronome)
+                                    .id("metronome")
+                            }
+                        }
                     }
-                    VStack {
-                        TunerView(
-                            pitchManager: pitchManager,
-                            noteConverter: noteConverter,
-                            isListening: $isListening,
-                            errorMessage: $errorMessage,
-                            currentPitch: $currentPitch,
-                            currentSpectrum: $currentSpectrum
-                        )
-                        .id("tuner")
-                        MetronomeView(metronome: metronome)
-                            .id("metronome")
-                    }
-                    
                 }
             }
-            
-    }
+        }
     }
 }
 
